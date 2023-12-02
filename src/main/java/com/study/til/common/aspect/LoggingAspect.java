8f68f124fb4afb5ad0abd4d350611371a2d3c2e7
KWY <wodrk1001@gmail.com>
2023-12-02 14:56:58 +0900

    test aspect 구현

--- src/main/java/com/study/til/common/aspect/LoggingAspect.java
+++ src/main/java/com/study/til/common/aspect/LoggingAspect.java
@@ -1,4 +1,62 @@
 package com.study.til.common.aspect;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Slf4j
 public class LoggingAspect {
+
+    @Around("execution(* com.study.til.controller..*(..))")
+    public Object requestLogging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
+        long startAt = System.currentTimeMillis();
+        Object returnValue = proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
+        long endAt = System.currentTimeMillis();
+        log.info("================================================NEW===============================================");
+        log.info("====> Request: {} {} ({}ms)\n *Header = {}", request.getMethod(), request.getRequestURL(), endAt - startAt, getHeaders(request));
+        if (returnValue != null) {
+            log.info("====> Response: {}", returnValue);
+        }
+        log.info("================================================END===============================================");
+        return returnValue;
+    }
+
+    @Around("execution(* com.study.til.common.advice..*(..))")
+    public Object requestWarnLogging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
+        long startAt = System.currentTimeMillis();
+        Object returnValue = proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
+        long endAt = System.currentTimeMillis();
+        log.info("================================================NEW===============================================");
+        log.info("====> Request: {} {} ({}ms)\n *Header = {}", request.getMethod(), request.getRequestURL(), endAt - startAt, getHeaders(request));
+        if (returnValue != null) {
+            log.info("====> Response: {}", returnValue);
+        }
+        log.info("================================================END===============================================");
+        return returnValue;
+    }
+
+    private Map<String, Object> getHeaders(HttpServletRequest request) {
+        Map<String, Object> headerMap = new HashMap<>();
+
+        Enumeration<String> headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
 }
